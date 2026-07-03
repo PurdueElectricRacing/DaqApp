d146964d7ea3802e4306d0042dbf3e29e74e4e9c
LelsersLasers <millankumar@gmail.com>
2026-07-02 22:19:59 -0700

    CorrelationFunction::correlate does not convert using timezones

--- src/daq_log_parse/correlate.rs
+++ src/daq_log_parse/correlate.rs
@@ -18,20 +18,9 @@ pub struct CorrelationFunction {
 }
 
 impl CorrelationFunction {
-    pub fn correlate(&self, log_ts: u32) -> Option<chrono::DateTime<chrono::Local>> {
+    pub fn correlate(&self, log_ts: u32) -> Option<chrono::DateTime<chrono::Utc>> {
         let unix_ms = self.slope * log_ts as f64 + self.intercept_ms;
-
-        match chrono::DateTime::from_timestamp_millis(unix_ms.round() as i64) {
-            Some(dt) => Some(dt.with_timezone(&chrono::Local)),
-            None => {
-                log::error!(
-                    "Correlated time {} ms for log time {} ms is out of range for chrono::DateTime",
-                    unix_ms,
-                    log_ts
-                );
-                None
-            }
-        }
+        chrono::DateTime::from_timestamp_millis(unix_ms.round() as i64)
     }
 }
 
